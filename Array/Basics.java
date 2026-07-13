313f98cd622d34ae5fc8b5f845a98a9aef6573db
Ashutosh Patil <ashutoshpatil1919@gmail.com>
2026-07-13 11:52:15 +0530

    Add files via upload

---
+++ Array/Basics.java
@@ -0,0 +1,23 @@
+package Array.Array2D;
+
+// Printing 2D array:
+
+public class Basics {
+    public static void main(String[] args) {
+        int arr[][] = new int[][] {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 },
+                { 17, 18, 19, 20 }
+        };
+        System.out.println("The 2D Array is:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
